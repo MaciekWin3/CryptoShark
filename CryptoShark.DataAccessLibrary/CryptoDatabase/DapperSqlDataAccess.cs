84331b302aef1553041e704c2b62b5b3ffca1b34
maciekwin3 <maciekwin3@gmail.com>
2021-04-20 12:40:36 +0200

    Switching to new db access

---
+++ CryptoShark.DataAccessLibrary/CryptoDatabase/DapperSqlDataAccess.cs
@@ -0,0 +1,37 @@
+﻿using CryptoShark.DataAccessLibrary.Models;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace CryptoShark.DataAccessLibrary.CryptoDatabase
+{
+    class DapperSqlDataAccess
+    {
+        private IDbConnection db;
+        public DapperSqlDataAccess(IConfiguration configuration)
+        {
+            this.db = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        //UserData
+
+        public UserDataModel Find(string Email)
+        {
+            var sql = "SELECT * FROM UserData WHERE Email = @Email";
+            return db.Query<UserDataModel>(sql, new { @Email = Email }).Single();
+        }
+
+        public List<UserDataModel> GetAll()
+        {
+            var sql = "SELECT * FROM UserData";
+            return db.Query<UserDataModel>(sql).ToList();
+        }
+
+
+    }
+}
